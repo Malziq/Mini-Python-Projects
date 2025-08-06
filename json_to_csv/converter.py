1144496162f543fc76d776d5fce3f136fab83ba8
Malziq <95142867+Malziq@users.noreply.github.com>
2025-08-06 12:58:05 -0700

    Update converter.py

--- json_to_csv/converter.py
+++ json_to_csv/converter.py
@@ -1,2 +1,16 @@
+import json
+
 if __name__ == "__main__":
-  main()
+  try:
+    with open("input.json", "r") as f:
+      data = json.loads(f.read())
+
+    output = ', '.join([*data[0]])
+    for obj in data:
+      output += f"\n{obj["Name"]}, {obj["age"]}, {obj["birthyear"]}"
+
+    with open("output.csv", "w") as f:
+      f.write(output)
+
+  except Exception as ex:
+    print(f"Error: {str(ex)}")
